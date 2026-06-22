0effde55cfd5a96125278f950053a9e3a7b69a50
Ethan Knapp <eknapp@instructure.com>
2026-06-22 13:33:50 -0600

    remove deprecated `active_support/configurable` usage
    Change-Id: Ic1b10db3122f4055ceebddf265a73bf197f83f7c
    Reviewed-on: https://gerrit.instructure.com/c/footrest/+/414735
    Reviewed-by: Caleb Siebach <caleb.siebach@instructure.com>
    Tested-by: Ethan Knapp <eknapp@instructure.com>
    QA-Review: Ethan Knapp <eknapp@instructure.com>
    Product-Review: Ethan Knapp <eknapp@instructure.com>

--- lib/footrest/client.rb
+++ lib/footrest/client.rb
@@ -1,21 +1,34 @@
 require 'footrest/connection'
 require 'footrest/request'
-require 'active_support/configurable'
+require 'active_support/ordered_options'
 
 module Footrest
   class Client
     include Footrest::Connection
     include Footrest::Request
-    include ActiveSupport::Configurable
 
-    config_accessor :token
-    config_accessor :prefix
-    config_accessor :logging
+    # Well-known options surfaced as reader/writer methods (e.g. client.token).
+    # Any other option remains reachable through #config (e.g. config[:domain]).
+    CONFIG_ACCESSORS = %i[token prefix logging].freeze
 
     def initialize(options={}, &block)
-      self.config.merge!(options)
+      config.merge!(options)
       yield self if block_given?
       set_connection(config)
+    end
+
+    # Per-instance options bag. ActiveSupport::OrderedOptions supports both
+    # hash-style (config[:token]) and method-style (config.token) access, the
+    # same surface ActiveSupport::Configurable's config object provided. This
+    # replaces `include ActiveSupport::Configurable`, which activesupport
+    # deprecates in 8.1 and removes in 8.2.
+    def config
+      @config ||= ActiveSupport::OrderedOptions.new
+    end
+
+    CONFIG_ACCESSORS.each do |name|
+      define_method(name) { config[name] }
+      define_method("#{name}=") { |value| config[name] = value }
     end
 
     def connection(&block)
--- lib/footrest/version.rb
+++ lib/footrest/version.rb
@@ -1,3 +1,3 @@
 module Footrest
-  VERSION = '0.6.0' unless defined?(Footrest::VERSION)
+  VERSION = '0.6.1' unless defined?(Footrest::VERSION)
 end
